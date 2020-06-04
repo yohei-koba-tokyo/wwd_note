83bd5628437148198c6cea6a02641f5e4a5c9c99
yohei-koba-tokyo <60637308+yohei-koba-tokyo@users.noreply.github.com>
2020-06-05 00:16:14 +0900

    relationship_function(server) completed

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,5 +1,5 @@
 class MemosController < ApplicationController
   def index
-    
+    @users = User.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'memos#index'
   resources :memos
+  resources :relationships, only: [:create, :destroy]
 end
