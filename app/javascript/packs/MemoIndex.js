e8b699ca36b5db22d5efb632f7150e7f117019d9
yohei-koba-tokyo <60637308+yohei-koba-tokyo@users.noreply.github.com>
2020-06-27 22:37:10 +0900

    change the style of the buttons depending on the situation.
    I've made it possible to change the style of the buttons depending on the situation.

--- app/javascript/packs/MemoIndex.js
+++ app/javascript/packs/MemoIndex.js
@@ -1,154 +1,97 @@
-/* eslint no-console: 0 */
-// Run this example by adding <%= javascript_pack_tag 'hello_vue' %> (and
-// <%= stylesheet_pack_tag 'hello_vue' %> if you have styles in your component)
-// to the head of your layout file,
-// like app/views/layouts/application.html.erb.
-// All it does is render <div>Hello Vue</div> at the bottom of the page.
-
-// import Vue from 'vue'
-// import App from '../app.vue'
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   const app = new Vue({
-//     render: h => h(App)
-//   }).$mount()
-//   document.body.appendChild(app.$el)
-
-//   console.log(app)
-// })
-
-
-// The above code uses Vue without the compiler, which means you cannot
-// use Vue to target elements in your existing html templates. You would
-// need to always use single file components.
-// To be able to target elements in your existing html/erb templates,
-// comment out the above code and uncomment the below
-// Add <%= javascript_pack_tag 'hello_vue' %> to your layout
-// Then add this markup to your html template:
-//
-// <div id='hello'>
-//   {{message}}
-//   <app></app>
-// </div>
-
 import axios from 'axios'
 import Vue from 'vue/dist/vue.esm'
 import App from '../app.vue'
 
-document.addEventListener('DOMContentLoaded', () => {
-  const app = new Vue({
-    el: '#memo',
-    data: {
-      memos: [],
-      page : 1,
-      perPage: 7,
-      totalPage: 0,
-      count: 0,
-      hoverFlag: false,
-      hoverIndex: null,
-      hoverUserFlag: false,
-      hoverUserIndex: null,
-      noteUrl: "",
-      users: [],
-    },
-    components: { App },
-    mounted () {
-      axios.get (
-        '/memos/pagenation.json'
-      )
-      .then(response =>{
-        this.memos = response.data;
-        this.count = response.data.length;
-        this.totalPage = Math.ceil(this.count / this.perPage);
-      });
-      axios.get (
-        '/api/relationships'
-      )
-      .then(response =>{
-        this.users = response.data;
-      });
-    },
-    methods: {
-      countUp: function() {
-        this.page= Math.max(this.page- 1, 1);
+if (document.getElementById('memo') !== null) {
+  document.addEventListener('DOMContentLoaded', () => {
+    const app = new Vue({
+      el: '#memo',
+      data: {
+        memos: [],
+        page : 1,
+        perPage: 7,
+        totalPage: 0,
+        count: 0,
+        hoverFlag: false,
+        hoverIndex: null,
+        hoverUserFlag: false,
+        hoverUserIndex: null,
+        noteUrl: "",
+        users: [],
       },
-      countDown: function() {
-        this.page= Math.min(this.page+ 1, this.totalPage);
-      },
-      createNoteNewUrl: function(id) {
-        this.noteUrl= "notes/new." + id
-      },
-      createNoteEditUrl: function(id) {
-        this.noteUrl= "notes/" + id + "/edit"
-      },
-      mouseOverAction: function(id) {
-        this.hoverFlag = true
-        this.hoverIndex = id
-      },
-      mouseLeaveAction: function() {
-        this.hoverFlag= false
-      },
-      mouseOverUser: function(id) {
-        this.hoverUserFlag = true
-        this.hoverUserIndex = id 
-      },
-      mouseLeaveUser: function() {
-        this.hoverUserFlag = false
-      },
-      followUser: function(id, own_id) {
-        axios.post (
-          '/api/relationships',
-          {
-            user_id: own_id,
-            follow_id: id
-          }
-        ).then(response => {
-          let pick = this.users.find(user => user.id == response.data.id)
-          pick.follow_id = response.data.follow_id
-        })
-        .catch(error => {
-          console.log(error);
+      components: { App },
+      mounted () {
+        axios.get (
+          '/memos/pagenation.json'
+        )
+        .then(response =>{
+          this.memos = response.data;
+          this.count = response.data.length;
+          this.totalPage = Math.ceil(this.count / this.perPage);
+        });
+        axios.get (
+          '/api/relationships'
+        )
+        .then(response =>{
+          this.users = response.data;
         });
       },
-      unfollowUser: function(id) {
-        axios.delete (
-          '/api/relationships/' + id,
-        ).then(response => {
-          let pick = this.users.find(user => user.id == response.data.id)
-          pick.follow_id = null
-        })
+      methods: {
+        countUp: function() {
+          this.page= Math.max(this.page- 1, 1);
+        },
+        countDown: function() {
+          this.page= Math.min(this.page+ 1, this.totalPage);
+        },
+        createNoteNewUrl: function(id) {
+          this.noteUrl= "notes/new." + id
+        },
+        createNoteEditUrl: function(id) {
+          this.noteUrl= "notes/" + id + "/edit"
+        },
+        mouseOverAction: function(id) {
+          this.hoverFlag = true
+          this.hoverIndex = id
+        },
+        mouseLeaveAction: function() {
+          this.hoverFlag= false
+        },
+        mouseOverUser: function(id) {
+          this.hoverUserFlag = true
+          this.hoverUserIndex = id 
+        },
+        mouseLeaveUser: function() {
+          this.hoverUserFlag = false
+        },
+        followUser: function(id, own_id) {
+          axios.post (
+            '/api/relationships',
+            {
+              user_id: own_id,
+              follow_id: id
+            }
+          ).then(response => {
+            let pick = this.users.find(user => user.id == response.data.id)
+            pick.follow_id = response.data.follow_id
+          })
+          .catch(error => {
+            console.log(error);
+          });
+        },
+        unfollowUser: function(id) {
+          axios.delete (
+            '/api/relationships/' + id,
+          ).then(response => {
+            let pick = this.users.find(user => user.id == response.data.id)
+            pick.follow_id = null
+          })
+        }
+      },
+      computed: {
+        filterMemos() {
+          return this.memos.slice((this.page - 1) * this.perPage, this.page * this.perPage);
+        }
       }
-    },
-    computed: {
-      filterMemos() {
-        return this.memos.slice((this.page - 1) * this.perPage, this.page * this.perPage);
-      }
-    }
+    })
   })
-})
-//
-//
-//
-// If the project is using turbolinks, install 'vue-turbolinks':
-//
-// yarn add vue-turbolinks
-//
-// Then uncomment the code block below:
-//
-// import TurbolinksAdapter from 'vue-turbolinks'
-// import Vue from 'vue/dist/vue.esm'
-// import App from '../app.vue'
-//
-// Vue.use(TurbolinksAdapter)
-//
-// document.addEventListener('turbolinks:load', () => {
-//   const app = new Vue({
-//     el: '#hello',
-//     data: () => {
-//       return {
-//         message: "Can you say hello?"
-//       }
-//     },
-//     components: { App }
-//   })
-// })
+}
--- app/javascript/packs/MemoIndexForm.js
+++ app/javascript/packs/MemoIndexForm.js
@@ -1,42 +1,49 @@
+
 import axios from 'axios'
 import Vue from 'vue/dist/vue.esm'
 import App from '../app.vue'
 
-document.addEventListener('DOMContentLoaded', () => {
-  const app = new Vue({
-    el: '#memo_form',
-    data: {
-      memos: []
-    },
-    components: { App },
-    mounted () {
-      axios.get (
-        '/api/memoforms'
-      )
-      .then(response =>{
-        this.memos = response.data
-      })
-    },
-    methods: {
-      changeMemos: function() {
-        // let memo = this.memos[0]
-        // axios.patch (
-        //   '/api/memoforms/' + memo.id,
-        //   {
-        //     memo: memo.memo
-        //   }
-        // )
-        this.memos.forEach(function(memo) {
-          axios.patch (
-            '/api/memoforms/' + memo.id,
-            {
-              memo: memo.memo
-            }
-          ).then(response => {
-            console.log(response)
-          })
+if (document.getElementById('memo_form') !== null) {
+  document.addEventListener('DOMContentLoaded', () => {
+    const app = new Vue({
+      el: '#memo_form',
+      data: {
+        memos: [],
+        copy: [],
+        change: true
+      },
+      components: { App },
+
+      mounted () {
+        axios.get (
+          '/api/memoforms'
+        )
+        .then(response =>{
+          this.memos = response.data
+          this.copy = JSON.parse(JSON.stringify(this.memos))
         })
+      },
+      methods: {
+        changeMemos: function() {
+          this.memos.forEach(function(memo) {
+            axios.patch (
+              '/api/memoforms/' + memo.id,
+              {
+                memo: memo.memo
+              }
+            ).then(response => {
+              console.log(response.config.data)
+            })
+          })
+        }
+      },
+      watch: {
+        memos: {
+          handler: function(val) {
+            this.change = JSON.stringify(val) == JSON.stringify(this.copy) ? true : false
+          },deep: true
+        }
       }
-    }
+    })
   })
-})
+}
